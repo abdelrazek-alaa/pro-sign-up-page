66e9b7f534019d0ab98844917d46a4ffb27d9aa6
abdelrazek <abdelrazekhdd@gmail.com>
2022-01-13 12:20:36 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import "./App.css";
 import SignUpForm from "./SignUpForm";
-import { badges } from "./data";
+import { badges, links, languages } from "./data";
 
 function App() {
   return (
     <div className="bg-main text-white space-y-6">
-      <div className="flex flex-col bg-yacht-Img bg-no-repeat bg-center py-6">
-        <header className="mb-7 sm:mb-0">
-          <button className="bg-white w-32 rounded p-2 m-4 float-right text-gray-800 font-semibold shadow-xl">
-            Log In
-          </button>
-        </header>
-        <main className="space-y-6">
-          <h1 className="text-4xl font-mono font-bold text-center">
-            My Product
-          </h1>
-          <SignUpForm />
-          <div className="space-y-4">
-            <h2 className="text-center text-2xl font-bold">
-              MY PRODUCT ON YOUR DEVICE
-            </h2>
-            <div className="flex flex-wrap justify-center">
-              {badges.map((badge) => (
-                <img
-                  key={badge.img}
-                  src={badge.img}
-                  alt=""
-                  className="h-12 mr-4 border-2  rounded-xl mt-3"
-                ></img>
-              ))}
+      <div className=" bg-yacht-Img bg-no-repeat bg-center ">
+        <div className="flex flex-col py-6 2xl:container 2xl:mx-auto">
+          <header className="mb-7 sm:mb-0">
+            <button className="bg-white w-32 rounded p-2 m-4 float-right text-gray-800 font-semibold shadow-xl">
+              Log In
+            </button>
+          </header>
+          <main className="space-y-6">
+            <h1 className="text-4xl font-mono font-bold text-center">
+              My Product
+            </h1>
+            <SignUpForm />
+            <div className="space-y-4 px-2">
+              <h2 className="text-center text-2xl font-bold">
+                MY PRODUCT ON YOUR DEVICE
+              </h2>
+              <div className="flex flex-wrap justify-center">
+                {badges.map((badge) => (
+                  <img
+                    key={badge.img}
+                    src={badge.img}
+                    alt=""
+                    className="h-12 mr-4 border-2  rounded-xl mt-3"
+                  ></img>
+                ))}
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
-      <footer>footer</footer>
+      <footer className="max-w-4xl mx-auto p-3 space-y-6">
+        {/* links */}
+        <ul className="flex flex-wrap justify-between sm:font-medium sm:text-lg">
+          {links.map((link) => (
+            <li key={link.link} className="m-2">
+              <a href={link.href}>{link.link}</a>
+            </li>
+          ))}
+        </ul>
+        <hr className="border-mainBorder" />
+        {/* language and copyright */}
+        <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0">
+          <select className="bg-main text-gray-400 border-[1px] border-mainBorder px-2 h-9 focus:border-sky-500 focus:border focus:outline-none sm:font-semibold rounded mr-6">
+            {languages.map((language) => (
+              <option key={language}>{language}</option>
+            ))}
+          </select>
+          <p>©2022 My Product Corporation.</p>
+        </div>
+      </footer>
     </div>
   );
 }
